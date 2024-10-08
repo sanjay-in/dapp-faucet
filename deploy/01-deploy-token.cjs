04c10344624cdca627349b7b4fbb45bf4b690db8
sanjay-in <sanjaysridharan@outlook.com>
2024-10-08 23:25:06 +0530

    add deploy script

---
+++ deploy/01-deploy-token.cjs
@@ -0,0 +1,32 @@
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat.config.cjs");
+const { default: verify } = require("../utils/verify.cjs");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = await deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const cap = 100000;
+  const initialSupply = 70000;
+  const blockReward = 10;
+
+  const args = [cap, initialSupply, blockReward];
+
+  log("Deploying Token contract...");
+
+  const token = await deploy("RedToken", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmation: 1,
+  });
+
+  log("Deployed contract");
+  log("..............................");
+
+  if (!developmentChains.includes(network.name)) {
+    await verify(token.address, args);
+  }
+};
+
+module.exports.tags = ["all", "token"];
---
+++ deploy/02-deploy-faucet.cjs
@@ -0,0 +1,35 @@
+const { network, ethers } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat.config.cjs");
+const { default: verify } = require("../utils/verify.cjs");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = await deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const contract = await deployments.get("RedToken");
+  const token = await ethers.getContractAt(contract.abi, contract.address);
+
+  const tokenAddress = token.address;
+  const lockTime = 10;
+  const withdrawAmount = 0.5;
+
+  const args = [tokenAddress, lockTime, withdrawAmount];
+
+  log("Deploying Faucet contract...");
+
+  const faucet = await deploy("Faucet", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmation: network.blockConfirmations,
+  });
+
+  log("Deployed contract");
+  log("..............................");
+
+  if (!developmentChains.includes(network.name)) {
+    await verify(faucet.address, args);
+  }
+};
+
+module.exports.tags = ["all", "token"];
