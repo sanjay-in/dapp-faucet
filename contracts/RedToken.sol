51d3aac2596fc2058e4464b6528408050850023e
sanjay-in <sanjaysridharan@outlook.com>
2024-10-09 23:04:03 +0530

    modify contract

--- contracts/RedToken.sol
+++ contracts/RedToken.sol
@@ -7,8 +7,8 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract RedToken is ERC20Capped {
     address payable public immutable i_owner;
-    uint256 private immutable i_decimals;
-    uint256 private s_blockReward;
+    uint256 public immutable i_decimals;
+    uint256 public s_blockReward;
 
     // Errors
     error RedToken__NotOwner();
@@ -34,33 +34,25 @@ contract RedToken is ERC20Capped {
         _mint(i_owner, initialSupply * i_decimals);
     }
 
-    function _mint(address account, uint256 value) internal virtual override {
+    function mint(address account, uint256 value) internal {
         if (totalSupply() + value > cap()) {
             revert RedToken__MaxCapExceeded();
         }
-        super._mint(account, value);
+        _mint(account, value);
     }
 
     function _mintMinerReward() internal {
-        _mint(block.coinbase, s_blockReward);
+        mint(block.coinbase, s_blockReward);
     }
 
-    function _transfer(
-        address from,
-        address to,
-        uint256 value
-    ) internal virtual override {
+    function transfer(address from, address to, uint256 value) internal {
         if (to != block.coinbase && totalSupply() + s_blockReward <= cap()) {
             _mintMinerReward();
         }
-        super._transfer(from, to, value);
+        _transfer(from, to, value);
     }
 
-    function setBlockReward(uint256 reward) external {
+    function setBlockReward(uint256 reward) external onlyOwner {
         s_blockReward = reward * i_decimals;
-    }
-
-    function destroy() external onlyOwner {
-        selfdestruct(payable(i_owner));
     }
 }
