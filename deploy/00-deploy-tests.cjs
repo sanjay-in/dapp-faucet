07a7c6d0d12d38f0226fad56c95a895e5654cd9c
sanjay-in <sanjaysridharan@outlook.com>
2024-10-10 21:13:25 +0530

    modify to deploy only on local

--- deploy/00-deploy-tests.cjs
+++ deploy/00-deploy-tests.cjs
@@ -1,24 +1,29 @@
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat.config.cjs");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, log } = await deployments;
-  const { deployer } = await getNamedAccounts();
+  if (developmentChains.includes(network.name)) {
+    const { deploy, log } = await deployments;
+    const { deployer } = await getNamedAccounts();
 
-  const cap = 100000;
-  const initialSupply = 70000;
-  const blockReward = 10;
+    const cap = 100000;
+    const initialSupply = 70000;
+    const blockReward = 10;
 
-  const args = [cap, initialSupply, blockReward];
+    const args = [cap, initialSupply, blockReward];
 
-  log("Deploying TestToken contract...");
+    log("Deploying TestToken contract...");
 
-  await deploy("TestToken", {
-    from: deployer,
-    args: args,
-    log: true,
-    waitConfirmation: 1,
-  });
+    await deploy("TestToken", {
+      from: deployer,
+      args: args,
+      log: true,
+      waitConfirmation: 1,
+    });
 
-  log("Deployed contract");
-  log("..............................");
+    log("Deployed contract");
+    log("..............................");
+  }
 };
 
 module.exports.tags = ["tests"];
