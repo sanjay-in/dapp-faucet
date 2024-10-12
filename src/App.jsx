ee3ac2f044ff2c66adb2536823530bb6d36d4878
sanjay-in <sanjaysridharan@outlook.com>
2024-10-12 12:23:27 +0530

    connect to wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import Home from "./components/Home/Home";
+import ConnectMetaMask from "./components/ConnectMetamask/ConnectMetamask";
+import { toastMessage } from "../utils/utils.mjs";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [connectMetamask, setConnectMetamask] = useState(false);
 
+  const connectMetaMask = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum
+          ?.request({
+            method: "eth_requestAccounts",
+          })
+          .then(() => {
+            setConnectMetamask(true);
+          })
+          .catch((error) => console.log(error));
+      } catch (error) {}
+    } else {
+      toastMessage("error", "No metamask extension found");
+    }
+  };
+  useEffect(() => {
+    connectMetaMask();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {connectMetamask ? <Home /> : <ConnectMetaMask connectMetaMask={connectMetaMask} />}
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
