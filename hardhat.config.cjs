7eb7d1dc689e761089a9bfc230a22ccb5a9b4e70
sanjay-in <sanjaysridharan@outlook.com>
2024-10-21 14:07:19 +0530

    update hardhat config file

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,7 +1,45 @@
-require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
+require("solidity-coverage");
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      blockConfirmations: 6,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    customChains: [],
+  },
+  gasReporter: {
+    enabled: false,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
 };
